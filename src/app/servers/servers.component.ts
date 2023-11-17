92af9f801df59c77ebc8ffa79ec3c37125c93096
Bilal-01 <mbilalaziz.01@gmail.com>
2023-11-17 19:42:57 +0500

    Angular: Section 2 - Bindings

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
@@ -6,6 +6,27 @@ import { Component } from '@angular/core';
   styleUrl: './servers.component.css'
 })
 
-export class ServersComponent {
+export class ServersComponent implements OnInit {
   
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'Test server';
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000)
+  }
+
+  ngOnInit(): void {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created!';
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
 }
